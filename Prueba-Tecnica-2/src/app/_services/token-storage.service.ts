aa39a074edc1774e0f1a4c86a6db9b61b920529d
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-09-19 11:35:20 +0200

    Update

--- Prueba-Tecnica-2/src/app/_services/token-storage.service.ts
+++ Prueba-Tecnica-2/src/app/_services/token-storage.service.ts
@@ -1,4 +1,11 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const BASE = 'https://api.themoviedb.org/';
+const headers = {
+  headers: new HttpHeaders(
+    { "authorization": "Bearer ae90c3bf26bbc28069a03d09e06bd844" })
+};
 
 @Injectable({
   providedIn: 'root'
--- Prueba-Tecnica-2/src/app/login/login.component.ts
+++ Prueba-Tecnica-2/src/app/login/login.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    username: null,
+    password: null
+  }
+  username?: string;
+  password?: string;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  TOKEN:string = "auth-token"
+  USERNAME:string = "auth-username"
 
   ngOnInit(): void {
+    if(window.sessionStorage.getItem(this.TOKEN))
+      this.router.navigate(['/home']);
+  }
+  onSubmit(): void{
+    console.log("LOGIN")
+    this.authService.login(this.form.username, this.form.password)
+      .subscribe(
+        response => {
+
+          window.sessionStorage.setItem(this.TOKEN, response.token);
+          window.sessionStorage.setItem(this.USERNAME, this.form.username);
+          console.log(window.sessionStorage.getItem(this.TOKEN))
+          console.log(window.sessionStorage.getItem(this.USERNAME));
+        },
+        error => {
+          // gestionar error
+          console.log(error)
+        });
   }
 
 }
